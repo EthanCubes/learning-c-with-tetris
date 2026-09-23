abb07355a6b332cd3c28ac02e805acc517c82efc
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-22 19:49:27 -0600

    Added window with raylib

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include "raylib.h"
 
 int main() {
-    printf("Hello world!\n");
+    InitWindow(540, 720, "Tetris Clone");
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
