1df386daeeed4b9bb14de0be94064022f9ca4ef7
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-23 17:46:56 -0600

    Added a thing that renders colors on the board

--- src/main.c
+++ src/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include "raylib.h"
 
+int board[10][20];
+const Color COLOR_ARRAY[7] = {
+    BLACK,
+    BLUE,
+    YELLOW,
+    PINK,
+    ORANGE,
+    GREEN,
+};
+
 void draw_board() {
     // the board is a 10x20 grid
     int tile_width = 24;
@@ -9,12 +19,13 @@ void draw_board() {
     int y_offset = (720 - 20*tile_height)/2;
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 20; y++) {
-            DrawRectangle(x*tile_width+x_offset, y*tile_height+y_offset, tile_width, tile_height, BLACK);
+            DrawRectangle(x*tile_width+x_offset, y*tile_height+y_offset, tile_width, tile_height, COLOR_ARRAY[board[x][y]]);
         }
     }
 }
 
 int main() {
+    board[7][6] = 1;
     InitWindow(720, 720, "Tetris Clone");
     while (!WindowShouldClose()) {
         BeginDrawing();
