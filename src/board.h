e4d072127b52d90e9f7b4b91a2c65750f3fec3bd
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-23 18:23:40 -0600

    Moved board changing code to own separate file

---
+++ src/board.h
@@ -0,0 +1,12 @@
+#ifndef BOARD
+#define BOARD
+
+#include "raylib.h"
+
+extern int board[10][20];
+
+extern const Color COLOR_ARRAY[7];
+
+void draw_board();
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,28 +1,6 @@
-#include <stdio.h>
 #include "raylib.h"
 
-int board[10][20];
-const Color COLOR_ARRAY[7] = {
-    BLACK,
-    BLUE,
-    YELLOW,
-    PINK,
-    ORANGE,
-    GREEN,
-};
-
-void draw_board() {
-    // the board is a 10x20 grid
-    int tile_width = 24;
-    int tile_height = 24;
-    int x_offset = (720 - 10*tile_width)/2;
-    int y_offset = (720 - 20*tile_height)/2;
-    for (int x = 0; x < 10; x++) {
-        for (int y = 0; y < 20; y++) {
-            DrawRectangle(x*tile_width+x_offset, y*tile_height+y_offset, tile_width, tile_height, COLOR_ARRAY[board[x][y]]);
-        }
-    }
-}
+#include "board.h"
 
 int main() {
     InitWindow(720, 720, "Tetris Clone");
