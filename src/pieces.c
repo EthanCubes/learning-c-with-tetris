08de7c42434cc64edf0c152d901485134c35bdbc
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-24 19:46:55 -0600

    Added code to check for an entire complete row.
    There isn't any way to clear the row yet, since I'm unsure how to shift the rest down.

--- src/pieces.c
+++ src/pieces.c
@@ -1,5 +1,30 @@
+#include <stdbool.h>
+
 #include "global.h"
 
 // This file is for piece movement, including handling user inputs
+
+bool is_falling(int x, int y) {
+    for (int block = 0; block < 4; block++) {
+        if (tetrimino_spots[block][0] == x && tetrimino_spots[block][1] == y) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void check_rows() {
+    for (int row = 0; row < 20; row++) {
+        int filled_squares = 0;
+        for (int column = 0; column < 10; column++) {
+            if (board[row][column] != 0 && !is_falling(row, column)) {
+                filled_squares++;
+            }
+        }
+        if (filled_squares == 10) {
+            // this needs to like move all the top squares down, idk how
+        }
+    }
+}
 
 void piece_calc() {}
