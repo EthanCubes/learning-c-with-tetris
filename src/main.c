f9a707e04404012b50f5e61def2ae57661a8ebc7
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-24 18:50:12 -0600

    Added piece calulation function to the main file

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 
 #include "global.h"
 #include "board.h"
+#include "pieces.h"
 
 int main() {
     InitWindow(720, 720, "Tetris Clone");
     while (!WindowShouldClose()) {
         // Simulation
+        piece_calc();
 
         // Rendering
         BeginDrawing();
