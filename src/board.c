0e76bdeb0ffefca100c081f0ac4de0bdb1c55139
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-24 20:54:37 -0600

    Added spacing to board in game

--- src/board.c
+++ src/board.c
@@ -17,11 +17,12 @@ void draw_board() {
     // the board is a 10x20 grid
     int tile_width = 24;
     int tile_height = 24;
-    int x_offset = (720 - 10*tile_width)/2;
-    int y_offset = (720 - 20*tile_height)/2;
+    int spacing = 1;
+    int x_offset = (720 - 10*(tile_width + spacing))/2;
+    int y_offset = (720 - 20*(tile_height + spacing))/2;
     for (int x = 0; x < 10; x++) {
         for (int y = 0; y < 20; y++) {
-            DrawRectangle(x*tile_width+x_offset, y*tile_height+y_offset, tile_width, tile_height, COLOR_ARRAY[board[x][y]]);
+            DrawRectangle(x * (tile_width + spacing) + x_offset, y * (tile_height + spacing) + y_offset, tile_width, tile_height, COLOR_ARRAY[board[x][y]]);
         }
     }
 }
