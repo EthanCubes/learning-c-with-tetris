368bc3c07e119edf9f6b68df7c49349d7af19696
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-26 12:07:08 -0600

    Added code that eventually will manage piece falling

--- src/pieces.c
+++ src/pieces.c
@@ -1,13 +1,30 @@
+#include <stdbool.h>
+#include <time.h>
+#include <stdlib.h>
+
 #include "raylib.h"
 
 #include "header/global.h"
 #include "header/clearing.h"
 #include "header/user_input.h"
 
-// This file is for piece movement, including handling user inputs
+bool currently_falling = false;
+
+void falling_pieces() {
+    if (currently_falling) {
+        // simulate the falling thing
+    }
+    else {
+        srand(time(NULL));
+        int r = rand()%10;
+        if (r = 0) {
+            // summon a falling piece
+        }
+    }
+}
 
 void piece_calc() {
     check_rows();
-    // simulate falling pieces
+    falling_pieces();
     user_input();
 }
