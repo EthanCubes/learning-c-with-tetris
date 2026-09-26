4b1038db9322bfc57fcae7f446ca8f4bc8d4433e
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-26 00:09:29 -0600

    Added alternative keybinds to some of the keys
    For example, W can be used instead of up arrow

--- src/pieces.c
+++ src/pieces.c
@@ -33,21 +33,21 @@ void check_rows() {
 
 void user_input() {
     // Mian movement keys
-    if (IsKeyDown(KEY_LEFT)) {
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
         // Move the falling piece left
     }
-    if (IsKeyDown(KEY_RIGHT)) {
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
         // Move thef allign piece right
     }
-    if (IsKeyDown(KEY_UP)) {
+    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
         // Flip the piece
     }
-    if (IsKeyDown(KEY_DOWN)) {
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
         // Accelerate
     }
 
     // Other miscellanious keys
-    if (IsKeyDown(KEY_ESCAPE)) {
+    if (IsKeyDown(KEY_ESCAPE) || IsKeyDown(KEY_P)) {
         // Pause the game
     }
     if (IsKeyDown(KEY_N)) {
@@ -58,5 +58,5 @@ void user_input() {
 void piece_calc() {
     check_rows();
     // simulate falling pieces
-    // get user input
+    user_input();
 }
