7fabd4101aaa5bce7927b7e3980d7423a055b132
EthanCubes <ethanshangyilin@protonmail.com>
2026-09-26 00:21:41 -0600

    Moved all header files into separate directory for organization

---
+++ src/header/pieces.h
@@ -0,0 +1,6 @@
+#ifndef PIECES
+#define PIECES
+
+void piece_calc();
+
+#endif
---
+++ src/header/render.h
@@ -0,0 +1,10 @@
+#ifndef RENDER
+#define RENDER
+
+extern int board[10][20];
+
+extern const Color COLOR_ARRAY[7];
+
+void draw_board();
+
+#endif
